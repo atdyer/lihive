7c6b58267b3158f0e73bd7821da4bf1124ba5cf8
Natereater <32504554+Natereater@users.noreply.github.com>
2022-02-16 13:17:02 -0800

    Fixed grasshopper movement to check for gates
    Grasshoppers were previously not checking for beetle gates on top of the hive. This should fix that.

--- packages/hive-lib/src/grasshopper.ts
+++ packages/hive-lib/src/grasshopper.ts
@@ -1,4 +1,4 @@
-import { eachDirection, isSpaceOccupied } from './board';
+import { eachDirection, isGated, isSpaceOccupied } from './board';
 import { hexesEqual, relativeHexCoordinate } from './hex';
 import { GameBoard, HexCoordinate } from './types';
 
@@ -20,10 +20,21 @@ export function getValidGrasshopperMoveCoordinates(
   eachDirection((direction) => {
     const neighbor = relativeHexCoordinate(coordinate, direction);
     let current = neighbor;
-    while (isSpaceOccupied(board, current)) {
+    // check if there is a gate between the grasshopper and first hex
+    var gateFound:boolean = isGated(board, coordinate, direction);
+    while (isSpaceOccupied(board, current) && !gateFound) {
+      // check for each gate along path
+      if (isGated(board, current, direction)) {
+        gateFound = true;
+      }
       current = relativeHexCoordinate(current, direction);
     }
-    if (!hexesEqual(current, neighbor)) {
+
+    // check for gate in last dropdown
+    if (isGated(board, current, direction)) {
+      gateFound = true;
+    }
+    if (!hexesEqual(current, neighbor) && !gateFound) {
       valid.push(current);
     }
   });
